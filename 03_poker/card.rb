3354c0bf1ce748635eb5a9e954a667dec7a87033
mrkchoi <kennethichoi@gmail.com>
2019-05-22 16:32:27 -0700

    Create Card class + Deck#build_shuffled_deck

--- 03_poker/card.rb
+++ 03_poker/card.rb
@@ -1,10 +1,17 @@
 
-class Card
+class Card  
+  attr_reader :suit, :value
+
   def initialize(suit, value)
     @suit = suit
     @value = value
   end
 
-  
-
+  def inspect
+    if suit == '♥' || suit == '♦'
+      "#{value}#{suit}".colorize(:red)
+    else
+      "#{value}#{suit}".colorize(:black)
+    end
+  end
 end
